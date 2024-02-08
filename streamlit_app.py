039d5af17236304c373dad07482030aa2bbf3b03
rwcolledge-snowflake <159152943+rwcolledge-snowflake@users.noreply.github.com>
2024-02-08 17:26:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ streamlit.dataframe(my_data_rows)
 
 # Allow user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ' + add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
